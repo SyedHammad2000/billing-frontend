618e73d0b5120a33760c8f1fffb33f9d92c1dad1
SyedHammad2000 <Hammadimran736@gmail.com>
2025-11-23 01:31:32 +0500

    updated

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://192.168.100.3:5000"
-    : "https://billing-backend-two.vercel.app";
+export const BASE_URL = "https://billing-backend-two.vercel.app";
