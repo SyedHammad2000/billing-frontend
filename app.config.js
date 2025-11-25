2d915a119a9563a28b4f9993baf865d411294bba
SyedHammad2000 <Hammadimran736@gmail.com>
2025-11-25 22:28:01 +0500

    updated

--- app.config.js
+++ app.config.js
@@ -7,62 +7,45 @@ export default ({ config }) => ({
   slug: "billing",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/images/hudwater.png",
-  scheme: "billing",
-  userInterfaceStyle: "automatic",
+
+  icon: "./assets/images/hudwater.png",  // APP ICON
+
   splash: {
-    image: "./assets/images/hudwater.png",
+    image: "./assets/images/splash-icon.png",
     resizeMode: "contain",
     backgroundColor: "#ffffff",
-    dark: {
-      backgroundColor: "#000000",
-    },
   },
+
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.hammad2000.billing",
   },
+
   android: {
     package: "com.hammad2000.billing",
     adaptiveIcon: {
       foregroundImage: "./assets/images/android-icon-foreground.png",
       backgroundImage: "./assets/images/android-icon-background.png",
-      backgroundColor: "#E6F4FE",
-      monochromeImage: "./assets/images/android-icon-monochrome.png",
     },
-
     usesCleartextTraffic: true,
-    edgeToEdgeEnabled: true,
-    predictiveBackGestureEnabled: false,
   },
+
   web: {
     bundler: "metro",
-    output: "static",
     favicon: "./assets/images/favicon.png",
   },
+
   plugins: [
     "expo-router",
-    [
-      "expo-splash-screen",
-      {
-        image: "./assets/images/splash-icon.png",
-        imageWidth: 200,
-        resizeMode: "contain",
-        backgroundColor: "#ffffff",
-        dark: {
-          backgroundColor: "#000000",
-        },
-      },
-    ],
+    ["expo-splash-screen", {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    }],
   ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true,
-  },
+
   extra: {
-    BASE_URL: isProd
-      ? "https://billing-backend-two.vercel.app"
-      : "https://billing-backend-two.vercel.app",
+    BASE_URL: "https://billing-backend-two.vercel.app",
     eas: {
       projectId: "a3eb0a71-c6d6-4452-a961-ce182c5f0e6e",
     },
